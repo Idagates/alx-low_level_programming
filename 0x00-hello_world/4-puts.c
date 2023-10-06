a27e849767b8e5a0dc7a189b80ba1c0d4ec071b8
Ida Seye <idamrsgates@gmail.com>
2023-10-05 21:12:18 -0700

    Task 4 and 5 done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * main - puts a statement followed by a new line
+ * Return: 0 end programme , 1 for error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - (A programme that prints a statement, followed by a new line)
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
