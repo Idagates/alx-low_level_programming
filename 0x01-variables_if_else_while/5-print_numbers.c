35136fe77c48eba927ff2f2644c951f4acc4fd45
Ida Seye <idamrsgates@gmail.com>
2023-10-06 22:42:38 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Return:0
+ */
+int main(void)
+{
+	int number = 0;
+
+	for ( ; number <= 9; number++)
+	{
+		printf("%d", number);
+	}
+	return (0);
+}
